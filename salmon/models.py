18d877f61bbbeed3c9f19ee63bc15d92c5545619
Paul Osman <paul@eval.ca>
2011-03-22 22:38:56 -0400

    Add a subscription model to store a salmon endpoint url with the associated object (feed item)

---
+++ salmon/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+
+
+class SubscriptionManager(models.Manager):
+
+    def subscribe(self, obj, endpoint):
+        content_type = ContentType.objects.get_for_model(obj)
+        return Subscription.objects.create(
+            content_type=content_type,
+            object_id=obj.id,
+            salmon_endpoint=endpoint)
+
+
+class Subscription(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    salmon_endpoint = models.URLField()
+
+    objects = SubscriptionManager()
+
+    def __unicode__(self):
+        obj = self.content_type.get_object_for_this_type(id=self.object_id)
+        return u":".join((
+            obj.__class__.__name__,
+            unicode(obj), self.salmon_endpoint))
+
+    def get_object(self):
+        cls = self.content_type.model_class()
+        return cls.objects.get(id=self.object_id)
