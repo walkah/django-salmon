73b4d3fa1d3a0415d1c15bcf140ad29adc7022d5
Paul Osman <paul@eval.ca>
2011-03-23 20:12:51 -0400

    Make salmon endpoint view csrf exempt

--- salmon/views.py
+++ salmon/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 
+@csrf_exempt
 @require_POST
 def endpoint(request):
-    return HttpResponse('endpoint')
+    return HttpResponse('slapped')
