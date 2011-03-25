b6a5805a5ed869f61de039d621a60a2d491312f9
Paul Osman <paul@eval.ca>
2011-03-25 00:38:29 -0400

    Use the in-progress magic signatures implementation to send notifications as magic envelopes

--- salmon/__init__.py
+++ salmon/__init__.py
@@ -1,32 +1,66 @@
-import urllib
+import urllib2
 import feedparser
 
+from django.conf import settings
+
+from salmon import magicsigs
 from salmon.models import Subscription
 
 
-def discover(url):
+def discover_salmon_endpoint(url):
+    """
+    Perform discovery on ``url``. Look for link[rel='salmon'] and
+    fetch the href.
+    """
     d = feedparser.parse(url)
     weblinks = getattr(d.feed, 'links', [])
     for link in weblinks:
-        if link.has_key('rel') and link['rel'] == 'salmon':
-            if link.has_key('href'):
+        link_dict = dict(link)
+        if 'rel' in link_dict and link_dict['rel'] == 'salmon':
+            if 'href' in link:
                 return link['href']
     return None
 
 
-def subscribe(obj, feed_url):
-    salmon_endpoint = discover(feed_url)
+def subscribe(feed, feed_url):
+    """
+    Perform discovery on feed to find salmon endpoint URI.
+
+    ``feed`` is the feed object, however it is represented in your system.
+    ``feed_url`` is the URL of the Atom / RSS feed.
+    """
+    salmon_endpoint = discover_salmon_endpoint(feed_url)
     if not salmon_endpoint:
         return None
-    return Subscription.objects.subscribe(obj, salmon_endpoint)
+    return Subscription.objects.subscribe(feed, salmon_endpoint)
 
 
-def unsubscribe(obj):
-    Subscription.objects.unsubscribe(obj)
+def unsubscribe(feed):
+    """Destroy a subscription to the feed object ``feed``."""
+    Subscription.objects.unsubscribe(feed)
 
 
 def slap(content, source):
+    """Notify a source of updated content."""
     sub = Subscription.objects.get_for_object(source)
-    if sub:
-        # just post some test data right now
-        urllib.urlopen(sub.salmon_endpoint, urllib.urlencode(dict(foo='bar')))
+    if not sub:
+        return  # nothing to do
+
+    # TODO(paulosman)
+    # hand waving on key storage right now. Just use a conf setting.
+    # eventually will have to get this associated with the user somehow.
+    key = getattr(settings, 'SALMON_USER_KEYPAIR', None)
+    if not key:
+        return
+
+    # hand waving on mime_type right now
+    magic_envelope = magicsigs.magic_envelope(
+        content, 'application/atom+xml', key)
+
+    # TODO(paulosman)
+    # really crappy HTTP client right now. Can improve when the basic
+    # protocol flow is working.
+    urllib2.urlopen(
+        sub.salmon_endpoint,
+        magic_envelope,
+    )
