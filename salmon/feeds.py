cab1d44ff5cd43ee918dc5ae71f87ac185a516a9
Paul Osman <paul@eval.ca>
2011-03-25 00:37:11 -0400

    Construct entry feeds (atom feeds where the root element is 'entry'

--- salmon/feeds.py
+++ salmon/feeds.py
@@ -1,6 +1,11 @@
+from StringIO import StringIO
+
 from django.core.urlresolvers import reverse
 from django.contrib.syndication.views import Feed
 from django.utils.feedgenerator import Atom1Feed
+from django.utils.xmlutils import SimplerXMLGenerator
+
+ATOM_NS = 'http://www.w3.org/2005/Atom'
 
 
 class SalmonAtom1Feed(Atom1Feed):
@@ -10,6 +15,40 @@ class SalmonAtom1Feed(Atom1Feed):
             handler.addQuickElement(u'link', '', {u'rel': u'salmon',
                                                   u'href': salmon})
         super(SalmonAtom1Feed, self).add_root_elements(handler)
+
+
+class SalmonAtom1EntryFeed(Atom1Feed):
+    def __init__(self, title, link, description, author_name,
+                 author_link, pubdate, **kwargs):
+        """
+        We don't create an atom:feed element, so don't force the caller
+        to pass in the same kwargs as are required by Atom1Feed's constructor.
+        Instead, mimic the signature of Atom1Feed.add_item with some stricter
+        requirements (author and updated aren't optional elements in Atom 1.0).
+        See: http://tools.ietf.org/html/rfc4287#section-4.1.2
+        """
+        super(SalmonAtom1EntryFeed, self).__init__(
+            title=u'',
+            link=u'',
+            description=u'')
+        self.add_item(title, link, description,
+                      author_name=author_name,
+                      author_link=author_link,
+                      pubdate=pubdate, **kwargs)
+
+    def item_attributes(self, item):
+        """Put the Atom namespace into the entry element."""
+        return {
+            'xmlns': ATOM_NS,
+        }
+
+    def __unicode__(self):
+        """Write out XML into a string buffer."""
+        sb = StringIO()
+        sb.write(u'<?xml version="1.0" encoding="utf-8"?>')
+        handler = SimplerXMLGenerator(out=sb)
+        self.write_items(handler)
+        return sb.getvalue()
 
 
 class SalmonFeed(Feed):
